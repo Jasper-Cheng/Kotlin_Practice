92d02e5ceaf82f37bf92950c3a5827b219686c0e
Jasper.Cheng <Jasper.Cheng@tymphany.com>
2020-12-02 15:05:27 +0800

    -changed minSdkVersion and AppTheme

--- app/src/main/java/com/example/particekotlin/MainActivity.kt
+++ app/src/main/java/com/example/particekotlin/MainActivity.kt
@@ -2,18 +2,11 @@ package com.example.particekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-
+s
 class MainActivity : AppCompatActivity() {
-    private lateinit var bt:Button
-    private var Jasper:String?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bt=findViewById(R.id.button)
-        bt.setOnClickListener{
-            Jasper="Jasper-Cheng"
-        }
     }
 }
